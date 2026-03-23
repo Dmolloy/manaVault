bc7602be699a3b3c039822a59503be9822cfc9af
Dmolloy <declanjmolloy@gmail.com>
2026-03-23 16:53:34 +0100

    Add registration page and update necessary links

--- manavault/urls.py
+++ manavault/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('profiles/', include('profiles.urls')),
 ]
 
 if settings.DEBUG:
