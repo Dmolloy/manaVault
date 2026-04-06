7a5eb5e3f7056428c2017b74139d89f890732f33
Dmolloy <declanjmolloy@gmail.com>
2026-04-06 19:08:02 +0200

    add cards page and changed placeholder links

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Card
 
 # Create your views here.
+
+def all_cards(request):
+    cards = Card.objects.filter(is_active=True)
+    return render(request, 'cards/cards.html', {'cards': cards})
--- manavault/urls.py
+++ manavault/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('cards/', include('cards.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('profiles/', include('profiles.urls')),
 ]
