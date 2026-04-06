292d1a55aeb4f64812f5d4079a74bc436ce6f540
Dmolloy <declanjmolloy@gmail.com>
2026-04-06 22:04:04 +0200

    create card detail page and ensure functionality

--- cards/urls.py
+++ cards/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import all_cards
+from .views import all_cards, card_detail
 
 urlpatterns = [
     path('', all_cards, name='cards'),
+    path('<int:card_id>/', card_detail, name='card_detail'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Card
 
 # Create your views here.
@@ -6,3 +6,8 @@ from .models import Card
 def all_cards(request):
     cards = Card.objects.filter(is_active=True)
     return render(request, 'cards/cards.html', {'cards': cards})
+
+
+def card_detail(request, card_id):
+    card = get_object_or_404(Card, pk=card_id, is_active=True)
+    return render(request, 'cards/card_detail.html', {'card': card})
