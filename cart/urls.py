7fc79c28db1fc5918cc8ba24feb7b7dafba2a2d4
Dmolloy <declanjmolloy@gmail.com>
2026-04-08 16:16:06 +0200

    add update/remove cards function in cart page

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import view_cart, add_to_cart
+from .views import view_cart, add_to_cart, update_cart, remove_from_cart
 
 urlpatterns = [
     path('', view_cart, name='view_cart'),
     path('add/<int:card_id>/', add_to_cart, name='add_to_cart'),
+    path('update/<int:card_id>/', update_cart, name='update_cart'),
+    path('remove/<int:card_id>/', remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -38,6 +38,36 @@ def add_to_cart(request, card_id):
 
     request.session['cart'] = cart
 
-    messages.success(request, f'Added {card.name} to your cart')
-
+    messages.success(request, f'{quantity} x {card.name} added to your cart.')
     return redirect('card_detail', card_id=card.id)
+
+def update_cart(request, card_id):
+    card = get_object_or_404(Card, pk=card_id)
+    quantity = int(request.POST.get('quantity', 1))
+    cart = request.session.get('cart', {})
+
+    if quantity > card.stock_quantity:
+        messages.error(request, f'Only {card.stock_quantity} of {card.name} available.')
+        return redirect('view_cart')
+
+    if quantity > 0:
+        cart[str(card_id)] = quantity
+        messages.success(request, f'Updated {card.name} quantity to {quantity}.')
+    else:
+        cart.pop(str(card_id), None)
+        messages.success(request, f'{card.name} removed from your cart.')
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+
+def remove_from_cart(request, card_id):
+    card = get_object_or_404(Card, pk=card_id)
+    cart = request.session.get('cart', {})
+
+    if str(card_id) in cart:
+        cart.pop(str(card_id))
+        request.session['cart'] = cart
+        messages.success(request, f'{card.name} removed from your cart.')
+
+    return redirect('view_cart')
