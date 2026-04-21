6faeaeb2f1dcfc701af223817ac70e7a51031b91
Dmolloy <declanjmolloy@gmail.com>
2026-04-21 21:50:46 +0200

    add filter to card page

--- cards/views.py
+++ cards/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Card
 
-# Create your views here.
 
 def all_cards(request):
     cards = Card.objects.filter(is_active=True)
-    return render(request, 'cards/cards.html', {'cards': cards})
+    selected_type = request.GET.get('type')
+
+    if selected_type:
+        cards = cards.filter(card_type__iexact=selected_type)
+
+    card_types = (
+        Card.objects.filter(is_active=True)
+        .values_list('card_type', flat=True)
+        .distinct()
+        .order_by('card_type')
+    )
+
+    return render(request, 'cards/cards.html', {
+        'cards': cards,
+        'card_types': card_types,
+        'selected_type': selected_type,
+    })
 
 
 def card_detail(request, card_id):
