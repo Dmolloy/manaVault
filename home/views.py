0ee4086c1ec94e9ea74cd5008577c743dfb99c6c
Dmolloy <declanjmolloy@gmail.com>
2026-04-22 16:09:11 +0200

    Temporarily simplify homepage for Heroku debugging

--- home/views.py
+++ home/views.py
@@ -1,23 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from cards.models import Card
-from .forms import ContactForm
-
+from django.shortcuts import render
 
 def index(request):
-    featured_cards = Card.objects.filter(is_active=True)[:3]
-    return render(request, 'home/index.html', {
-        'featured_cards': featured_cards,
-    })
-
-
-def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            messages.success(request, 'Thanks for your message. We will get back to you soon.')
-            return redirect('contact')
-    else:
-        form = ContactForm()
-
-    return render(request, 'home/contact.html', {'form': form})
+    return render(request, 'home/index.html')
