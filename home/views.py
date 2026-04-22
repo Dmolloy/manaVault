9d39df98c7a7a269531cad29520d70a2d9e4f211
Dmolloy <declanjmolloy@gmail.com>
2026-04-22 12:06:18 +0200

    add contact page with basic form and sucess message

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from cards.models import Card
+from .forms import ContactForm
 
 
 def index(request):
@@ -7,3 +9,15 @@ def index(request):
     return render(request, 'home/index.html', {
         'featured_cards': featured_cards,
     })
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Thanks for your message. We will get back to you soon.')
+            return redirect('contact')
+    else:
+        form = ContactForm()
+
+    return render(request, 'home/contact.html', {'form': form})
