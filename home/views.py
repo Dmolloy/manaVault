1789eaef6310ae1f22286b69a5b7cc2bef6081c8
Dmolloy <declanjmolloy@gmail.com>
2026-04-22 16:29:27 +0200

    Restore contact page after deployment fix

--- home/views.py
+++ home/views.py
@@ -1,4 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from .forms import ContactForm
+
 
 def index(request):
     return HttpResponse("ManaVault homepage works")
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Message sent successfully.')
+            return redirect('contact')
+    else:
+        form = ContactForm()
+
+    return render(request, 'home/contact.html', {'form': form})
